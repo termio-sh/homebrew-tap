bd77c3c38b5e8105c71d5fc1e41e1ff941e40369
termio-release <release@termio.sh>
2026-08-28 19:45:55 +0000

    termio 0.46.0

--- Casks/termio.rb
+++ Casks/termio.rb
@@ -1,6 +1,6 @@
 cask "termio" do
-  version "0.45.0"
-  sha256 "f1b6ca7f172b935450ea5dbfecb3ee03c6723d2366ff1d6b75aa8521c697cd78"
+  version "0.46.0"
+  sha256 "bdb08a040e1e259ab2b7f73cd30e2ea2d496c604f1a29c23e701aa1a6c087ebe"
 
   url "https://downloads.termio.sh/v#{version}/termio.dmg"
   name "termio"
